614a552d4374ceb59ddad41ca321343aecea5af8
LWtiffany <2577933967@qq.com>
2026-04-01 16:53:18 +0800

    fix: add timeout

--- Frontend/src/features/portfolio/services/portfolioService.js
+++ Frontend/src/features/portfolio/services/portfolioService.js
@@ -8,3 +8,8 @@ export const getPotentialData = () => api.get('/api/v1/dashboard/MeasurePotentia
 
 export const getDiversificationData = (type) =>
   api.get(`/api/v1/dashboard/ManageDiversification/${type}`);
+
+export const getRiskAssessment = () =>
+  api.get('/api/v1/dashboard/RiskAssessment', {
+    timeout: 15000,
+  });
